a531b4c89169093ea3d1258c42b765b17abb5be1
MatthieuEv <evainmatthieu44@gmail.com>
2021-11-30 17:34:24 +0100

    Ajout de couleur dans le menu

--- menu.py
+++ menu.py
@@ -1,12 +1,15 @@
 from outils import user_input
-from color import clear
+from color import clear,posXY,init
+from colorama import Fore, Back
 
 def menu():
+    init()
     while(1):
-        clear()
-        print("\n===== Labyrinthe Du Gladiateur =====\n")
-        print("• Choisir le labyrinthe (1)")
-        print("• Quitter le jeu (0)\n")
+        #clear()
+        posXY(0,10)
+        print(Fore.GREEN,"▬▬▬▬▬",Fore.RESET,Back.RESET,"Labyrinthe Du Gladiateur",Fore.GREEN,"▬▬▬▬▬",Fore.RESET,Back.RESET,"\n")
+        print("• Choisir le labyrinthe ",Fore.BLACK,Back.RED,"(1)",Fore.RESET,Back.RESET,sep="")
+        print("• Quitter le jeu ",Fore.BLACK,Back.RED,"(0)",Fore.RESET,Back.RESET,"\n",sep="")
         #run = soit 0, soit 1, pas autre choses
         run=user_input([0,1])
         #si run = 0, on quitte tout
@@ -14,9 +17,10 @@ def menu():
             exit()
         elif run == 1:
             clear()
+            posXY(0,10)
             print("\n===== Labyrinthe Du Gladiateur =====\n")
-            print("• Choisissez un labyrinthe entre 1 et 12")
-            print("• Quitter le choix du labyrithe (0)\n")
+            print("• Choisissez un labyrinthe entre ",Fore.BLACK,Back.RED,"1 et 12",Fore.RESET,Back.RESET,sep="")
+            print("• Quitter le choix du labyrithe ",Fore.BLACK,Back.RED,"(0)",Fore.RESET,Back.RESET,"\n",sep="")
             #choixLaby est compris entre 0 et 12
             choixLaby = user_input([0,1,2,3,4,5,6,7,8,9,10,11,12])
             if choixLaby != 0:
