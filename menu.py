84d934502c1ad4c620aca9ffdd97e059baf859ba
MatthieuEv <evainmatthieu44@gmail.com>
2021-11-30 17:36:02 +0100

    Hotfix couleur 2nd menu

--- menu.py
+++ menu.py
@@ -18,7 +18,7 @@ def menu():
         elif run == 1:
             clear()
             posXY(0,10)
-            print("\n===== Labyrinthe Du Gladiateur =====\n")
+            print(Fore.GREEN,"▬▬▬▬▬",Fore.RESET,Back.RESET,"Labyrinthe Du Gladiateur",Fore.GREEN,"▬▬▬▬▬",Fore.RESET,Back.RESET,"\n")
             print("• Choisissez un labyrinthe entre ",Fore.BLACK,Back.RED,"1 et 12",Fore.RESET,Back.RESET,sep="")
             print("• Quitter le choix du labyrithe ",Fore.BLACK,Back.RED,"(0)",Fore.RESET,Back.RESET,"\n",sep="")
             #choixLaby est compris entre 0 et 12
