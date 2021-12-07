105effbfc0e520a26b11662e85b07e870b3dfebe
LouvAndTech <leri.louvmer@hotmail.fr>
2021-12-07 16:31:15 +0100

    Adding pyGame start function

---
+++ pygameFile.py
@@ -0,0 +1,122 @@
+import pygame
+
+#Taille
+HAUTEUR = 720
+LARGEUR = 1280
+TAILLE_CARRE = 20
+#Couleurs
+WHITE = (255,255,255)
+LIGHTBLUE = (114,159,207)
+BLUE = (52,101,164)
+GREEN = (0,255,0)
+DARKGREEN = (78,154,6)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+PINK = (168,50,117)
+PURPLE = (119,50,168)
+
+"""
+Fonction pour crée la fenêtre 
+Argument :
+    - rien 
+Retourne :
+    - un object fenêtre
+"""
+def pgInit():
+    pygame.init()
+    return(pygame.display.set_mode((LARGEUR,HAUTEUR)))
+
+"""
+Fonction pour recuperer les event
+argument :
+    - rien 
+retourne :
+    - les events
+"""
+def getEvent():
+    return(pygame.event.get())
+
+"""
+Fonction pour mettre à jour l'ecran 
+argument : 
+    -rien 
+retourne :
+    - rien 
+"""
+def updateScreen():
+    pygame.display.flip()
+
+"""
+Fonction pour afficher le fond d'ecran
+argument:
+    - une fenêtre 
+retourne :
+    - rien
+"""
+def fondDecran(screen):
+    screen.fill(BLUE)
+    pygame.draw.rect(screen,LIGHTBLUE,pygame.Rect(30,30,(1280-60),(720-60)))
+
+"""
+Fonction qui affiche un lab 
+argument:
+    - une fenêtre 
+    - un lab (tableau a deux dim)
+retourne :
+    - rien 
+"""
+def pgAfficherLab(screen,lab):
+    dep = [LARGEUR//2 - (len(lab[0])//2)*TAILLE_CARRE,HAUTEUR//2 - (len(lab)//2)*TAILLE_CARRE]
+    for x in range(len(lab[0])) :
+        for y in range(len(lab)):
+            if lab[y][x]:
+                pygame.draw.rect(screen,BLACK,pygame.Rect(dep[0],dep[1],TAILLE_CARRE,TAILLE_CARRE))
+            elif (x%2==0 or y%2==0):
+                pygame.draw.rect(screen,DARKGREEN,pygame.Rect(dep[0],dep[1],TAILLE_CARRE,TAILLE_CARRE))
+            else:
+                pygame.draw.rect(screen,GREEN,pygame.Rect(dep[0],dep[1],TAILLE_CARRE,TAILLE_CARRE))
+
+            dep[1] = dep[1]+TAILLE_CARRE
+        dep = [dep[0]+TAILLE_CARRE,HAUTEUR//2 - (len(lab)//2)*TAILLE_CARRE]
+
+
+"""
+Fonction pour gérer le déplacement d'une entité sur le plalteau 
+Arguments:
+    - un objet screen
+    - La liste a deux dim contenant le tableau du lab
+    - un entier contenant l'entite a afficher :
+        - 0 = gladiateur
+        - 1 = player
+    - position de d'arrive = liste a deux dim X-Y 
+    - position de départ = liste a deux dim X-Y 
+        //Par defaut a -1 si pas de valeur de départ//
+retourne :
+    rien 
+"""
+def pgDeplacementEntite(screen,tabLab,entite,posArr,posDep=[-1,-1]):
+    dep = [LARGEUR//2 - (len(tabLab[0])//2)*TAILLE_CARRE,HAUTEUR//2 - (len(tabLab)//2)*TAILLE_CARRE]
+    if (posDep[0]!=-1):
+        pygame.draw.rect(screen,GREEN,pygame.Rect(dep[0]+TAILLE_CARRE*posDep[0],dep[1]+TAILLE_CARRE*posDep[1],TAILLE_CARRE,TAILLE_CARRE))
+    if entite :
+        pygame.draw.rect(screen,YELLOW,pygame.Rect(dep[0]+TAILLE_CARRE*posArr[0],dep[1]+TAILLE_CARRE*posArr[1],TAILLE_CARRE,TAILLE_CARRE))
+    else:
+        pygame.draw.rect(screen,RED,pygame.Rect(dep[0]+TAILLE_CARRE*posArr[0],dep[1]+TAILLE_CARRE*posArr[1],TAILLE_CARRE,TAILLE_CARRE))
+
+"""
+Fonction pour changer le personnage a la fin du jeu
+argument: 
+    - un object screen
+    - liste a deux dim contenant le tableau 
+    - liste avec les position pour afficher l'emoji
+    - Un boolean a True si victoire
+retourne :
+    - rien 
+"""
+def  pgGraphEndGame(screen,tabLab,posafficher,vict):
+    dep = [LARGEUR//2 - (len(tabLab[0])//2)*TAILLE_CARRE,HAUTEUR//2 - (len(tabLab)//2)*TAILLE_CARRE]
+    if vict : 
+        pygame.draw.rect(screen,PURPLE,pygame.Rect(dep[0]+TAILLE_CARRE,posafficher[0],dep[1]+TAILLE_CARRE*posafficher[1],TAILLE_CARRE,TAILLE_CARRE))
+    else:
+        pygame.draw.rect(screen,PINK,pygame.Rect(dep[0]+TAILLE_CARRE*posafficher[0],dep[1]+TAILLE_CARRE*posafficher[1],TAILLE_CARRE,TAILLE_CARRE))
