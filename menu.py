b4594b16208a7c64e8fac017570c1226bed39890
matthieuEv <evainmatthieu44@gmail.com>
2021-12-07 17:19:16 +0100

    ajout de la partie création de save

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import colorama
-from outils import user_input
+from colorama.initialise import reset_all
+from outils import read_save_file, user_input, user_input_str,testSaveExist, write_save_file
 from color import clear,posXY,init,clear_down,background
 from colorama import Fore, Back
 
@@ -11,12 +12,16 @@ def menu():
         background()
         posXY(20,11)
         print(Back.LIGHTBLUE_EX,Fore.BLACK,"▬▬▬▬▬ Labyrinthe Du Gladiateur ▬▬▬▬▬")
+
         posXY(23,13)
-        print("• Choisir le labyrinthe (1)")
+        print("• Choisir le mode Arcade (1)")
         posXY(23,14)
+        print("• Choisir le mode Histoire (2)")
+        posXY(23,15)
         print("• Quitter le jeu (0)")
         #run = soit 0, soit 1, pas autre choses)
-        run=user_input([0,1],isPos=True,pos=[23,17])
+        run=user_input([0,1,2],isPos=True,pos=[23,18])
+
         #si run = 0, on quitte tout
         if run == 0:
             print(colorama.Style.RESET_ALL)
@@ -45,6 +50,46 @@ def menu():
                 clear()
                 return choixLaby
             #si choixLaby = 0, retour au menu de base
+
+        elif run == 2:
+            print(colorama.Style.RESET_ALL)
+            clear()
+            background()
+            posXY(20,11)
+            print(Back.LIGHTBLUE_EX,Fore.BLACK,"▬▬▬▬▬ Labyrinthe Du Gladiateur ▬▬▬▬▬")
+            profils = read_save_file("save")
+            posXY(23,13)
+            print("• Choisir une save (1)")
+            posXY(23,14)
+            print("• Créer une save (2)")
+            posXY(23,15)
+            print("• Quitter menu (0)")
+            #run = soit 0, soit 1, pas autre choses)
+            choose=user_input([0,1,2],isPos=True,pos=[23,18])
+            print(colorama.Style.RESET_ALL)
+            if choose == 1:
+                print(colorama.Style.RESET_ALL)
+                clear()
+                background()
+                posXY(20,11)
+                input("continue")
+            if choose == 2: #créer la save
+                print(colorama.Style.RESET_ALL)
+                clear()
+                background()
+                posXY(20,11)
+                print(Back.LIGHTBLUE_EX,Fore.BLACK,"▬▬▬▬▬ Labyrinthe Du Gladiateur ▬▬▬▬▬")
+                posXY(23,13)
+                print("• Nom de la save:")
+                while True:
+                    saveName=user_input_str([23, 15])
+                    if testSaveExist("save",saveName) == False:
+                        break
+                profils.append([saveName,1])
+                print(profils)
+                write_save_file("save",profils)
+                clear()
+                print(colorama.Style.RESET_ALL)
 
 
 def menu_joueur_jeu(dir_possible):
